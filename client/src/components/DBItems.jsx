cc6a19f2055a1b7d6d4fcf99595294310d2504eb
Arunraj003 <arunr6506@gmail.com>
2024-07-15 10:17:07 +0530

    commit "p"

--- client/src/components/DBItems.jsx
+++ client/src/components/DBItems.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteAProduct, getAllProducts } from "../api";
 import { HiCurrencyRupee } from "../assets/icons";
@@ -7,8 +7,20 @@ import { alertNULL, alertSuccess } from "../context/actions/alertActions";
 import { setAllProducts } from "../context/actions/productActions";
 
 const DBItems = () => {
-  const products = useSelector((state) => state.products);
   const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
+
+  // Fetch products on component mount
+  useEffect(() => {
+    getAllProducts()
+      .then((data) => {
+        dispatch(setAllProducts(data));
+      })
+      .catch((error) => {
+        console.error("Failed to fetch products:", error);
+      });
+  }, [dispatch]);
+
   return (
     <div className="flex items-center justify-self-center gap-4 pt-6 w-full">
       <DataTable
@@ -20,6 +32,7 @@ const DBItems = () => {
               <img
                 src={rowData.imageURL}
                 className="w-32 h-16 object-contain rounded-md"
+                alt="Product"
               />
             ),
           },
@@ -35,14 +48,14 @@ const DBItems = () => {
             title: "Price",
             field: "product_price",
             render: (rowData) => (
-              <p className="text-xl font-semibold text-textColor flex items-center justify-center ">
+              <p className="text-xl font-semibold text-textColor flex items-center justify-center">
                 <HiCurrencyRupee className="text-red-400" />
                 {parseFloat(rowData.product_price).toFixed(2)}
               </p>
             ),
           },
         ]}
-        data={products}
+        data={products || []}
         title="List of Products"
         actions={[
           {
@@ -56,18 +69,21 @@ const DBItems = () => {
             icon: "delete",
             tooltip: "Delete Data",
             onClick: (event, rowData) => {
-              if (
-                window.confirm("Are you sure, you want to perform this aciton")
-              ) {
-                deleteAProduct(rowData.productId).then((res) => {
-                  dispatch(alertSuccess("Product Deleted "));
-                  setInterval(() => {
-                    dispatch(alertNULL());
-                  }, 3000);
-                  getAllProducts().then((data) => {
+              if (window.confirm("Are you sure you want to perform this action?")) {
+                deleteAProduct(rowData.productId)
+                  .then((res) => {
+                    dispatch(alertSuccess("Product Deleted"));
+                    setTimeout(() => {
+                      dispatch(alertNULL());
+                    }, 3000);
+                    return getAllProducts();
+                  })
+                  .then((data) => {
                     dispatch(setAllProducts(data));
+                  })
+                  .catch((error) => {
+                    console.error("Failed to delete product or fetch updated products:", error);
                   });
-                });
               }
             },
           },
